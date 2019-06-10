cd311f8d3c23c2a040627cbe177dd460cdd5ee59
Surya Kiran <surya1994@icloud.com>
2019-06-10 23:29:29 +0100

    first commit in github

---
+++ Avinash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Avinash
+//
+//  Created by Apple on 10/06/19.
+//  Copyright © 2019 suryacompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
